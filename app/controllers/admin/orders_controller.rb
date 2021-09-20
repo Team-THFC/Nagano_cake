bc08d3d549d1200e5c44f6a21fed25b4f090fa84
ishizukamaaya <maayajas@gmail.com>
2021-09-20 09:36:57 +0000

    orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,14 +3,22 @@ class Admin::OrdersController < ApplicationController
   def show
     @orders = Order.all
     @order = Order.find(params[:id])
-    @orders_params = OrderProduct.all
-   
+    @orders_products = @order.order_products
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(orders_params)
+      redirect_to admin_order_path(@order),notice: "注文ステータス変更しました"
+    else
+      render :show
+    end
   end
 
   private
 
   def orders_params
-    params.require(:order).permit(:member_id,:shipping_price,:total_payment,:payment_method,:name,:address,:postal_code,:status)
+    params.require(:order).permit(:status)
   end
 
 end
