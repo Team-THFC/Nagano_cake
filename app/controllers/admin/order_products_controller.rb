3180f975dc5033e0e9ecb2885f75507a6211efa8
ishizukamaaya <maayajas@gmail.com>
2021-09-24 13:22:55 +0000

    orders-css

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -3,11 +3,15 @@ class Admin::OrderProductsController < ApplicationController
   def update
     @order_product = OrderProduct.find(params[:id])
     @order_product.update(order_product_params)
-    @order_products = @order_product.order.order_products
+    order = @order_product.order
+    @order_products = order.order_products
+    order_products_completed = @order_products.where(status: "製作完了")
     if @order_product.production_status == "製作中"
-      @order_product.order.update(status: "製作中")
-    elsif @order_products.all? {|order_product| order_product.production_status == "製作完了" }
-      @order_product.order.update(status: "発送準備中")
+      @order_product.order.status = "製作中"
+      @order_product.order.save
+    elsif @order_products.count == order_products_completed.count
+      @order_product.order.status = "発送準備中"
+      @order_product.order.save
     end
     redirect_to admin_order_path(@order_product.order)
   end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,11 +8,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order_products = @order.order_products
     @order.update(order_params)
-
+    @order_products = @order.order_products
     if @order.status == "入金確認"
-        @order_products.update_all(production_status: "制作待ち")
+      @order_products.each do |order_product|
+        order_product.production_status = "制作待ち"
+        order_product.save
+      end
     end
     redirect_to admin_order_path(@order)
   end
