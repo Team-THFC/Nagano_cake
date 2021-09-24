75454ef2abc3f43a3c0274855b924a27bb21cb8c
ishizukamaaya <maayajas@gmail.com>
2021-09-24 04:28:25 +0000

    order-status

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,13 +1,17 @@
 class Admin::OrderProductsController < ApplicationController
 
   def update
-    @order_product = OrderProduct.find_by(id: params[:id], order_id: params[:order_id])
-    if @order_product.update(order_product_params)
-      redirect_to admin_order_path(@order_product.order),notice: "制作ステータス変更しました"
-    else
-      render :show
+    @order_product = OrderProduct.find(params[:id])
+    @order_product.update(order_product_params)
+    @order_products = @order_product.order.order_products
+    if @order_product.production_status == "製作中"
+      @order_product.order.update(status: "製作中")
+    elsif @order_products.all? {|order_product| order_product.production_status == "製作完了" }
+      @order_product.order.update(status: "発送準備中")
     end
+    redirect_to admin_order_path(@order_product.order)
   end
+
 
   private
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,11 +8,13 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    if @order.update(order_params)
-      redirect_to admin_order_path(@order),notice: "注文ステータス変更しました"
-    else
-      render :show
+    @order_products = @order.order_products
+    @order.update(order_params)
+
+    if @order.status == "入金確認"
+        @order_products.update_all(production_status: "制作待ち")
     end
+    redirect_to admin_order_path(@order)
   end
 
   private
